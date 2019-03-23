86d0eb050852780de6c74c4bde2e3bf13e0a58bb
Dane Mackier <dane.mackier@outlook.com>
2019-03-23 09:54:04 +0200

    Fixed up the initial bits to do what we need it to do.

--- lib/smart_flare.dart
+++ lib/smart_flare.dart
@@ -4,7 +4,9 @@
 library smart_flare;
 
 import 'package:flutter/material.dart';
+import 'package:flare_flutter/flare.dart';
 import 'package:flare_flutter/flare_actor.dart';
+import 'package:flare_flutter/flare_controls.dart';
 
 /// A wrapper to the FlareActor that provides additional user input functionality.
 /// By identifying certain areas on the animation we can assign it callbacks to trigger or
@@ -33,7 +35,9 @@ class SmartFlareActor extends StatefulWidget {
 }
 
 class _SmartFlareActorState extends State<SmartFlareActor> {
-  String _animationName;
+  String _lastPlayedAnimation;
+
+  final FlareControls _animationControls = FlareControls();
 
   @override
   Widget build(BuildContext context) {
@@ -41,15 +45,18 @@ class _SmartFlareActorState extends State<SmartFlareActor> {
       print('SmartFlare:Warning: - No starting animation supplied');
     }
 
-    print('SmartFlare: ${_animationName ?? widget.startingAnimation}');
+    // print('SmartFlare: ${_animationName ?? widget.startingAnimation}');
 
     return GestureDetector(
       onTapUp: _handleTapUp,
       child: Container(
         width: widget.width,
         height: widget.height,
-        child: FlareActor(widget.filename,
-            animation: _animationName ?? widget.startingAnimation),
+        child: FlareActor(
+          widget.filename,
+          controller: _animationControls,
+          animation: widget.startingAnimation,
+        ),
       ),
     );
   }
@@ -62,17 +69,7 @@ class _SmartFlareActorState extends State<SmartFlareActor> {
     widget.activeAreas.forEach((activeArea) {
       // Check if the current touch is in the local position
       if (activeArea.area.contains(localPosition)) {
-        if (activeArea.animationName != null) {
-          setState(() {
-            _animationName = activeArea.animationName;
-          });
-        }
-
-        if (activeArea.animationsToCycle != null) {
-          setState(() {
-            _animationName = activeArea.getNextAnimation();
-          });
-        }
+        playAnimation(activeArea);
 
         if (activeArea.onAreaTapped != null) {
           activeArea.onAreaTapped();
@@ -80,6 +77,24 @@ class _SmartFlareActorState extends State<SmartFlareActor> {
       }
     });
   }
+
+  void playAnimation(ActiveArea activeArea) {
+    String animationToPlay;
+
+    if (activeArea.animationName != null) {
+     animationToPlay = activeArea.animationName;
+    } else if (activeArea.animationsToCycle != null) {
+     animationToPlay = activeArea.getNextAnimation();
+    }
+
+      if(activeArea.hasRequiredAnimation && _lastPlayedAnimation ==activeArea.animationRequired) {
+        print('SmartFlare:Info - Last played animation is $_lastPlayedAnimation and $animationToPlay has a guard against it');
+        return;
+      }
+     _animationControls.play(animationToPlay);
+     _lastPlayedAnimation = animationToPlay;
+  }
+
 }
 
 /// This model represents an area ontop of our Smart Flare actor that
@@ -88,15 +103,21 @@ class ActiveArea {
   /// The area that the ActiveArea represents
   final Rect area;
 
-  /// The name of the animation to play when a user has tapped on this
+  /// The name of the animation to play when a user taps on this
   /// area.
   final String animationName;
 
-  /// A list of the animations to cycle through when a user taps this area
+  /// A list of the animations to cycle through when a user taps this area.
+  /// It cycles through from 0 to the end of the list. One animation per tap
+  /// This cannot be used together with animation name.
   final List<String> animationsToCycle;
 
   /// This callback will be fired when the animation area is interacted with
   final Function onAreaTapped;
+
+  /// Set this value to the required animation that needs to be active
+  /// in order for this animation to play
+  final String animationRequired;
 
   int _nextAnimationIndex = 0;
 
@@ -104,18 +125,31 @@ class ActiveArea {
       {@required this.area,
       this.animationsToCycle,
       this.animationName,
-      this.onAreaTapped}) {
+      this.onAreaTapped,
+      this.animationRequired}) {
     if (animationName == null && onAreaTapped == null) {
       print(
           'SmartFlare:Warning - If you want some feedback from the animation add either animationName or onAreaTapped');
     }
+
+    var hasAnimationsToCycleAndAnimationName =
+        this.animationName != null && this.animationsToCycle != null;
+    assert(!hasAnimationsToCycleAndAnimationName,
+        'Use either animationsToCycle or animationName but not both');
+
+    if (animationsToCycle == null) {
+      print(
+          'SmartFlare:Warning - If you are cycling through animations there\'s a high chance that you might need to set the animationRequired property on the Active area.');
+    }
   }
 
+  bool get hasRequiredAnimation => animationRequired != null;
+
   String getNextAnimation() {
-    var nextAnimation =animationsToCycle[_nextAnimationIndex];
+    var nextAnimation = animationsToCycle[_nextAnimationIndex];
     _nextAnimationIndex++;
 
-    if(_nextAnimationIndex ==animationsToCycle.length) {
+    if (_nextAnimationIndex == animationsToCycle.length) {
       _nextAnimationIndex = 0;
     }
 
